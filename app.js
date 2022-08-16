cb2820e79c8b1ecb9090358132fbf9845fb8b260
Kyran Doyle <kyrandoyle1@gmail.com>
2022-08-16 11:51:56 +1000

    Added sounds to selected bars

--- app.js
+++ app.js
@@ -11,13 +11,38 @@ class DrumKit {
   repeater() {
     const step = drumKit.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      //Check if bar is active
+      if (bar.classList.contains("active")) {
+        //Check which sound to play
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
     drumKit.index++;
     console.log(activeBars);
   }
 
   start() {
     const interval = (60 / this.bpm) * 1000;
-    setInterval(this.repeater, interval);
+    setInterval(() => {
+      this.repeater();
+    }, interval);
+  }
+  activePad() {
+    console.log(this);
+    this.classList.toggle("active");
   }
 }
 
@@ -27,4 +52,11 @@ const drumKit = new DrumKit();
 
 drumKit.playBtn.addEventListener("click", function () {
   drumKit.start();
+});
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
